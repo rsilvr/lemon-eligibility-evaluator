aaecfd800310390e70e71e7410bbe448952f064f
Ricardo Silverio <rsilverio@fastmail.com>
2025-04-09 09:15:13 -0300

    error middlewares

--- src/app.js
+++ src/app.js
@@ -9,20 +9,20 @@ const app = express()
 app.use(express.json())
 
 app.post('/eligibility', (req, res) => {
-  try {
-    validateInput(req.body)
-    const payload = assembleEligibilityRequest(req.body)
-    const response = evaluateEligibility(payload)
-    return res.status(200).json(response)
-  } catch (e) {
-    if (e.status && e.erro) {
-      return res.status(e.status).json({
-        erro: e.erro,
-        detalhe: e.detalhe,
-      })
-    }
-    return res.status(500).json({ erro: constants.errorMessages.internalError })
-  }
+  validateInput(req.body)
+  const payload = assembleEligibilityRequest(req.body)
+  const response = evaluateEligibility(payload)
+  return res.status(200).json(response)
+})
+
+app.use((err, req, res, next) => {
+  if (err.status && err.erro) return res.status(err.status).json({ erro: err.erro, detalhe: err.detalhe })
+  next(err)
+})
+
+app.use((err, req, res, _next) => {
+  if (err.name === 'SyntaxError') return res.status(400).json({ erro: constants.errorMessages.invalidRequest })
+  return res.status(500).json({ erro: constants.errorMessages.internalError })
 })
 
 module.exports = app
